296a3b2fc3b354011562e5d3902a5d46e1490b28
Lthiumm <santibrrz091@gmail.com>
2026-06-07 13:35:19 +0200

    added new block, the bearing axle

---
+++ src/main/java/com/lycoris/loconautics/content/bearingaxle/BearingAxleBlock.java
@@ -0,0 +1,42 @@
+package com.lycoris.loconautics.content.bearingaxle;
+
+import com.lycoris.loconautics.registry.LoconauticsRegistries;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import org.jetbrains.annotations.Nullable;
+
+public class BearingAxleBlock extends Block implements EntityBlock {
+
+    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+
+    public BearingAxleBlock(BlockBehaviour.Properties properties) {
+        super(properties);
+        registerDefaultState(stateDefinition.any().setValue(FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+        return new BearingAxleBlockEntity(LoconauticsRegistries.BEARING_AXLE_BE.get(), pos, state);
+    }
+}
---
+++ src/main/java/com/lycoris/loconautics/content/bearingaxle/BearingAxleBlockEntity.java
@@ -0,0 +1,21 @@
+package com.lycoris.loconautics.content.bearingaxle;
+
+import com.simibubi.create.foundation.blockEntity.SmartBlockEntity;
+import com.simibubi.create.foundation.blockEntity.behaviour.BlockEntityBehaviour;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+
+import java.util.List;
+
+public class BearingAxleBlockEntity extends SmartBlockEntity {
+
+    public BearingAxleBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
+        super(type, pos, state);
+    }
+
+    @Override
+    public void addBehaviours(List<BlockEntityBehaviour> behaviours) {
+        // Kinetic behaviours wired in Phase 2
+    }
+}
